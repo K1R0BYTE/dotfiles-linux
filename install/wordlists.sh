96b08bbf2d599b897216fe99e7e32650cf4a3d0c
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 14:57:22 +0600

    Create wordlists.sh

---
+++ install/wordlists.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+RED="\033[1;31m"
+NOCOLOR="\033[0m"
+
+colorecho() {
+	echo -e "[*] ${RED}${1}${NOCOLOR}"
+}
+
+colorecho 'Starting Installing Wordlists'
+
+sudo git clone "https://github.com/danielmiessler/SecLists.git" /opt/SecLists
+
+colorecho 'Finished Installing Wordlists'
