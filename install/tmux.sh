7b36bd97bd52debbcb4faad59bfc8ba877116206
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 12:31:08 +0600

    Update tmux.sh

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt install tmux xclip -y
+sudo apt install tmux xclip -y
 rm -rf ${HOME}/.tmux*
 git clone "https://github.com/tmux-plugins/tpm" ${HOME}/.tmux/plugins/tpm
 ln -sv ${HOME}/.dotfiles/tmux/.tmux.conf ${HOME}/.tmux.conf
