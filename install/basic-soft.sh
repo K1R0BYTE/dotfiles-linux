1b02608a6af1200fb42f6f34e1b0b382d50da3c0
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 12:36:16 +0600

    Update basic-soft.sh

--- install/basic-soft.sh
+++ install/basic-soft.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+RED="\033[1;31m"
+NOCOLOR="\033[0m"
+
+colorecho() {
+	echo -e "[*] ${RED}${1}${NOCOLOR}"
+}
+
 sudo apt update && sudo apt install git vim wget curl -y
 
 # Sublime Text
@@ -7,4 +14,6 @@ sudo apt update && sudo apt install git vim wget curl -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install apt-transport-https -y
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update && apt install sublime-text -y
+sudo apt update && sudo apt install sublime-text -y
+
+colorecho 'Finished Installing Basic Soft'
