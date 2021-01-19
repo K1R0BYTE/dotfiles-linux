40f40a566c545e53e9c80390824abfc5d3d20582
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 12:28:30 +0600

    Update basic-soft.sh

--- install/basic-soft.sh
+++ install/basic-soft.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-apt update && apt install git vim wget -y
+sudo apt update && sudo apt install git vim wget curl -y
 
 # Sublime Text
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-apt install apt-transport-https -y
+sudo apt install apt-transport-https -y
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt update && apt install sublime-text -y
+sudo apt update && apt install sublime-text -y
