2170c8d5e3cdb731fc41dcd20d9d1b5c81dfbbb4
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 12:37:32 +0600

    Update tmux.sh

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+RED="\033[1;31m"
+NOCOLOR="\033[0m"
+
+colorecho() {
+	echo -e "[*] ${RED}${1}${NOCOLOR}"
+}
+
 sudo apt install tmux xclip -y
 rm -rf ${HOME}/.tmux*
 git clone "https://github.com/tmux-plugins/tpm" ${HOME}/.tmux/plugins/tpm
 ln -sv ${HOME}/.dotfiles/tmux/.tmux.conf ${HOME}/.tmux.conf
 tmux source-file ${HOME}/.tmux.conf
+
+colorecho 'Finished Installing Basic Soft'
