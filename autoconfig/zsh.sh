182adca112dd8a16a580e923083c09a7ff4fb02d
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 10:35:44 +0600

    Update zsh.sh

--- autoconfig/zsh.sh
+++ autoconfig/zsh.sh
@@ -13,3 +13,4 @@ ZSH_CUSTOM="$ZSH/custom"
 sudo apt install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/jimeh/zsh-peco-history.git ${ZSH_CUSTOM}/plugins/zsh-peco-history
