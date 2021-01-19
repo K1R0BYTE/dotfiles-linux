52a5550a4013debe393ce519f40630e4becc7ee7
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 11:24:15 +0600

    Create python.sh

---
+++ autoconfig/python.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+RED="\033[1;31m"
+NOCOLOR="\033[0m"
+
+colorecho() {
+	echo -e "[*] ${RED}${1}${NOCOLOR}"
+}
+
+ZSH="${HOME}/.oh-my-zsh"
+ZSH_CUSTOM="$ZSH/custom"
+
+# Python3 (pip + venv)
+apt install python3-pip -y
+python3 -m pip install --upgrade pip
