27b5b2ee594c9c73c0d558f190f5d60663f64e7d
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 14:19:10 +0600

    Update basic-soft.sh

--- install/basic-soft.sh
+++ install/basic-soft.sh
@@ -11,4 +11,9 @@ colorecho 'Starting Installing Basic Soft'
 
 sudo apt update && sudo apt install git vim wget curl -y
 
+curl https://github.com/peco/peco/releases/download/v0.2.0/peco_linux_amd64.tar.gz -o /tmp/peco.tar.gz
+cd /tmp
+tar -zxvf peco.tar.gz
+dpkg -i *.deb
+
 colorecho 'Finished Installing Basic Soft'
