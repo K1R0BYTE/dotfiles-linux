742963a2a03cb6fd3a6a5599ffa673c2cedb17a5
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 10:26:49 +0600

    Create zsh.sh

---
+++ autoconfig/zsh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+RED="\033[1;31m"
+NOCOLOR="\033[0m"
+
+colorecho() {
+	echo -e "[*] ${RED}${1}${NOCOLOR}"
+}
+
+ZSH="${HOME}/.oh-my-zsh"
+ZSH_CUSTOM="$ZSH/custom"
+
+sudo apt install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
