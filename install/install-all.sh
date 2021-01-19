eee39e5355fd872850dbfc1f67ac21fafc9e280b
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 16:04:20 +0600

    Update install-all.sh

--- install/install-all.sh
+++ install/install-all.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Resolve DOTFILES_DIR
+if [ -d "$HOME/.dotfiles" ]; then
+    DOTFILES_DIR="$HOME/.dotfiles"
+else
+    echo "Unable to find dotfiles, exiting..."
+    return
+fi
+
+# Source dotfiles
+for DOTFILE in "$DOTFILES_DIR"/system/.*; do
+    [ -f "$DOTFILE" ] && . "$DOTFILE"
+done
+
 # System
 bash system.sh
 
