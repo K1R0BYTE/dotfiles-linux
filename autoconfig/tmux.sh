fc77ff9f3d5558931b07fe38818399a90e95265d
K1R0BYTE <77654555+K1R0BYTE@users.noreply.github.com>
2021-01-19 11:19:47 +0600

    Create tmux.sh

---
+++ autoconfig/tmux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt install tmux xclip -y
+rm -rf ${HOME}/.tmux*
+git clone "https://github.com/tmux-plugins/tpm" ${HOME}/.tmux/plugins/tpm
+ln -sv ${HOME}/.dotfiles/tmux/.tmux.conf ${HOME}/.tmux.conf
+tmux source-file ${HOME}/.tmux.conf
