5be41e7d95133e73035781a162b0ceaf6077a7bd
K1R0BYTE <kirill.murzin@gmail.com>
2021-01-22 16:03:14 +0600

    small changes

--- install/go.sh
+++ install/go.sh
@@ -12,9 +12,4 @@ colorecho 'Starting Installing GoLang Soft'
 curl https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz -o /tmp/go.tar.gz
 sudo tar -C /usr/local -xzf /tmp/go.tar.gz
 
-export GO111MODULE=on
-export GOPATH="/opt/go"
-export PATH="$PATH:/usr/local/go/bin"
-export PATH=$PATH:$(go env GOPATH)/bin
-
 colorecho 'Finished Installing GoLang Soft'
